df20f1971ca8cdedfc7aa5561e5d5f73ac465980
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2023-05-08 21:12:29 +0600

    solve problem 2

--- TypeScript/src/problem-solving.ts
+++ TypeScript/src/problem-solving.ts
@@ -14,5 +14,32 @@ function problem1(params1: string, params2: number = 3): void {
 
 }
 
-problem1("without number");
-problem1("wit number", 10);
+// problem1("without number");
+// problem1("wit number", 10);
+// ----------- end -------------- 
+
+
+/* 
+
+Problem 2 🧨🧨
+Write a function that takes in an array of objects with properties name and age, and returns only the objects where the person's age is greater than or equal to 18,
+
+*/
+
+function problem2(params: any): void {
+    params.map((data: any) => {
+
+        if (data.age >= 18) {
+            console.log(data);
+        }
+    })
+}
+
+type arrayObj = {
+    name: string,
+    age: number
+}
+
+const arryObject: arrayObj[] = [{ name: 'abul', age: 13 }, { name: 'kabul', age: 22 }]
+
+problem2(arryObject);
