78d9d1c8714433b910a4fcf9c958371cdbfe1c6a
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2023-05-13 23:45:27 +0600

    crete Interface and schema

--- Mongoose/src/app.ts
+++ Mongoose/src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
+import { Schema } from 'mongoose';
 import cors from "cors";
 
 
@@ -7,7 +8,57 @@ app.use(cors());
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send('Mongoose server! ✌️✌️')
-    next();
+
+    // Create Interface
+    interface IUser {
+        name: {
+            firstName: string;
+            lastName: string;
+        }
+        roll: number;
+        address: {
+            homeAddress: string;
+            permanentAddress: string;
+        },
+        gender: "Male" | "Female";
+    }
+
+    // Create Schema
+    const userSchema = new Schema<IUser>({
+        name: {
+            firstName: {
+                type: String,
+                required: true,
+            },
+            lastName: {
+                type: String,
+                required: true,
+            }
+        },
+        roll: {
+            type: Number,
+            required: true,
+            unique: true
+
+
+        },
+        address: {
+            homeAddress: {
+                type: String,
+                required: true
+            },
+            permanentAddress: {
+                type: String,
+                required: true
+            }
+        },
+        gender:{
+            type: String,
+            required: true,
+            enum: ['Male', 'Female']
+        }
+    });
+
 })
 
 
